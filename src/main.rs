7bc5b3d91c58e510e7e321081640aaf339cb41fc
Martin Beckmann <martin@froglogic.com>
2021-08-15 17:02:58 +0200

    Add starting point for HttpServer
    This is the starting point from any actix web server. Because the
    different REST endpoints will have to read and write the same state I
    went ahead an already implemented the server state.
    It is just a concurrent HashMap mapping from a domain to the indexed
    URLs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(async_closure)]
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use chashmap::CHashMap;
+
+#[derive(Debug, Clone, Default)]
+struct AppState {
+    pub crawled_pages: CHashMap<String, Vec<String>>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app_state = web::Data::new(AppState::default());
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
